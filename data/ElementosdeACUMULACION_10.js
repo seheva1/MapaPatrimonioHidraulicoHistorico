62bf989d965d0a4c46678668b8c84cb31dd556c2
seheva1 <hernamperez.s@hotmail.com>
2018-05-07 12:00:21 +0200

    Subida 1

---
+++ data/ElementosdeACUMULACION_10.js
@@ -0,0 +1,11 @@
+var json_ElementosdeACUMULACION_10 = {
+"type": "FeatureCollection",
+"name": "ElementosdeACUMULACION_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ELEMENTO": "Lago", "NOMBRE": "Lago del Bosque Finlandes", "MUNICIPIO": "Rascafria", "Ficha": "https:\/\/drive.google.com\/open?id=1a1s3JiAH4xoX4bq9j8M0lyMCwQeyfZbX", "Imagen": "F:\\Fotos recortadas\\Ya recortadas\\IMG_3959.jpg" }, "geometry": { "type": "Point", "coordinates": [ -3.883783723305246, 40.890006035518716 ] } },
+{ "type": "Feature", "properties": { "ELEMENTO": "Presa", "NOMBRE": "Presa de Puentes Viejas", "MUNICIPIO": "Paredes de Buitrago", "Ficha": "https:\/\/drive.google.com\/open?id=1KYBtareWtTQX9Kcf_PxKBuPnsDqI8ak2", "Imagen": "F:\\Fotos recortadas\\Ya recortadas\\IMG_6348.jpg" }, "geometry": { "type": "Point", "coordinates": [ -3.571273162999935, 40.993196992000037 ] } },
+{ "type": "Feature", "properties": { "ELEMENTO": "Presa", "NOMBRE": "Presa del Villar", "MUNICIPIO": "Manjiron", "Ficha": "https:\/\/drive.google.com\/open?id=1peXqz8nYalfjOCA-sAYT6P5wiiM__cM4", "Imagen": "F:\\Fotos recortadas\\Ya recortadas\\IMG_4198.jpg" }, "geometry": { "type": "Point", "coordinates": [ -3.562890564501534, 40.94760827331924 ] } },
+{ "type": "Feature", "properties": { "ELEMENTO": "Presa", "NOMBRE": "Las Presillas", "MUNICIPIO": "Rascafria", "Ficha": "https:\/\/drive.google.com\/open?id=1rMv3AzlUiZXNw91u3A8mu4ANjtcEy3zW", "Imagen": "F:\\Fotos recortadas\\Ya recortadas\\IMG_3935.jpg" }, "geometry": { "type": "Point", "coordinates": [ -3.879119820609193, 40.880164699860075 ] } }
+]
+}
